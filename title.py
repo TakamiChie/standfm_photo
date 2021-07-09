78aea53ec21aa5dd029ebac5f8769d3fecb011d0
TakamiChie@Visnu <chie@onpu-tamago.net>
2021-07-09 23:30:37 +0900

    設定を外部ファイルに移行。モードに対応

--- title.py
+++ title.py
@@ -9,23 +9,22 @@ from jinja2 import Environment, FileSystemLoader
 from selenium import webdriver
 import chromedriver_binary
 from mutagen.easyid3 import EasyID3
+import yaml
 
 from tksugar import Generator
 
-THEME_TEXT = {
-  0: "プログラミング",
-  1: "SBC.Web配信",
-  2: "フリートーク",
-  3: "きまぐれライブ",
-  4: "今週の予定",
-  5: ["ゲームについて", "アニメについて", "地域について", "SBC.について", "フリートーク"],
-  6: "きまぐれライブ",
-}
+with open("config.yml", "r", encoding="utf-8") as f:
+  config = yaml.safe_load(f)
+  for n, v in config["path"].items():
+    config["path"][n] = Path(os.path.expandvars(v))
+
 TODAY = datetime.date.today()
-MP3DIR = Path(os.environ["OneDriveConsumer"]) / "stand.fm"
+MP3DIR = Path(config["path"]["basefolder"])
+THEME_TEXT_DEF = config["themes"]["dailythemes"]
+THEME_MODE_DEF = config["themes"]["modethemes"]
 
 parser = ArgumentParser()
-parser.add_argument("--theme", default=THEME_TEXT[TODAY.weekday()], help="テーマを変更する。" )
+parser.add_argument("--theme", default=THEME_TEXT_DEF[TODAY.weekday()], help="テーマを変更する。" )
 parser.add_argument("--title", help="タイトル。未指定時はファイルからの取得を試みる。")
 parser.add_argument("--date", default=TODAY, type=lambda s: datetime.datetime.strptime(s, "%Y/%m/%d"), help="yyyy/mm/dd形式の日付。指定がない場合は今日の日付。")
 parser.add_argument("--mode", help="モードの値。未指定時モード無し")
@@ -35,7 +34,9 @@ parser.add_argument("--bgm", help="この値は無視されます")
 args = parser.parse_args()
 
 fnpattern = "%Y-%m-%d"
-if args.mode: fnpattern += f"_{args.mode}"
+if args.mode:
+  fnpattern += f"_{args.mode}"
+  args.theme = THEME_MODE_DEF[args.mode]
 fnpattern += ".mp3"
 mp3path = MP3DIR / args.date.strftime(fnpattern)
 title = "名称未設定"
@@ -72,7 +73,8 @@ data = {
   'day' : args.date.strftime("%Y/%m/%d"),
   'week': "{0}曜日".format("月火水木金土日"[args.date.weekday()]),
   'theme': args.theme[args.date.day // 7] if type(args.theme) is list else args.theme,
-  'title': args.title
+  'title': args.title,
+  'mode': args.mode
 }
 out = Path(__file__).parent / "html" / "out.html"
 with open(out, mode="w", encoding="utf-8") as f:
